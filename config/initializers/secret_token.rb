3be1c04bc39ae145c22001d6cc00312592216f2f
sigabort <abort.coredump@gmail.com>
2010-06-24 11:23:34 -0400

    Adding automatr stubs created by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '5718e0462d1c8bcc0fa3ec3e6bd6c9d726cf1c73827179f29e9b3f9e220accd510f97f283b05d2bf8f676e1428d439881bab4155f5e8c2cdb6ac2fa3da11af9d'
