53b8cd6fe8a7a879927cefbc3b8471bedb39a7f3
sigabort <abort.coredump@gmail.com>
2010-06-25 16:11:02 -0400

    Adding blueprint css, and adding some layouts

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -1,8 +1,27 @@
 class InstitutesController < ApplicationController
+  respond_to :html, :json, :xml
+
   def new
+    @title = "Create Institute"
+    @institute = Institute.new
+    respond_with(@institute)
+  end
+
+  def create
+    @institute = Institute.new(params[:institute])
+    if @institute.save
+      redirect_to(@institute)
+    else
+      @title = "Create Institute"
+      render "new"
+    end
   end
 
   def show
+    @institute = Institute.find(params[:id])
+    respond_with(@institute) unless @institue.nil?
+  rescue ActiveRecord::RecordNotFound
+    flash.now[:error] = "Invalid Institute ID"
   end
 
   def index
